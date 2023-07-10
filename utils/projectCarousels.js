48af0c7654efc4fa49da7997de2db9c9ef2441b8
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-10 20:02:07 +0200

    getting close to final touches

---
+++ utils/projectCarousels.js
@@ -0,0 +1,19 @@
+// const carouselContainer = document.querySelector(".carousel-container");
+// const carousel = document.querySelector(".carousel");
+// let scrollStartPosition = 0;
+
+// carouselContainer.addEventListener("scroll", handleScroll);
+
+// function handleScroll(event) {
+//   console.log("helllooo");
+
+//   const scrollPosition = carouselContainer.scrollLeft;
+
+//   if (scrollPosition > scrollStartPosition) {
+//     carousel.scrollLeft += 10; // Adjust the scroll speed as desired
+//   } else if (scrollPosition < scrollStartPosition) {
+//     carousel.scrollLeft -= 10; // Adjust the scroll speed as desired
+//   }
+
+//   scrollStartPosition = scrollPosition;
+// }
