a6d779a32e9e62844b4b6f087afea0256f5c45e2
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-11 10:30:38 +0200

    hello

--- utils/projectCarousels.js
+++ utils/projectCarousels.js
@@ -1,19 +1,97 @@
-// const carouselContainer = document.querySelector(".carousel-container");
-// const carousel = document.querySelector(".carousel");
-// let scrollStartPosition = 0;
+const projects = [
+  {
+    title: "Pupper palette",
+    image: "../assets/images/beagle.png",
+    description: "This is the first project.",
+    liveLink: "https://pupper-d9d8b.web.app/",
+    codeLink: "https://github.com/username/project1",
+  },
+  {
+    title: "I made a promise",
+    image: "../assets/images/IMadeAPromise.png",
+    description: "This is the second project.",
+    liveLink: "",
+    codeLink: "https://github.com/username/project2",
+  },
+  {
+    title: "Wiz Sort",
+    image: "../assets/images/WizSort.png",
+    description: "This is the second project.",
+    liveLink: "https://live-link-2.com",
+    codeLink: "https://github.com/username/project2",
+  },
+  {
+    title: "4",
+    image: "assets/images/project2.png",
+    description: "This is the second project.",
+    liveLink: "https://live-link-2.com",
+    codeLink: "https://github.com/username/project2",
+  },
+  {
+    title: "5",
+    image: "assets/images/project2.png",
+    description: "This is the second project.",
+    liveLink: "https://live-link-2.com",
+    codeLink: "https://github.com/username/project2",
+  },
+];
 
-// carouselContainer.addEventListener("scroll", handleScroll);
+class Carousel {
+  constructor(projects, carouselElement, nextButton, prevButton) {
+    this.projects = projects;
+    this.carouselElement = carouselElement;
+    this.nextButton = nextButton;
+    this.prevButton = prevButton;
+    this.currentIndex = 0;
 
-// function handleScroll(event) {
-//   console.log("helllooo");
+    this.updateCarousel();
 
-//   const scrollPosition = carouselContainer.scrollLeft;
+    this.nextButton.addEventListener("click", () => this.nextProject());
+    this.prevButton.addEventListener("click", () => this.prevProject());
+  }
 
-//   if (scrollPosition > scrollStartPosition) {
-//     carousel.scrollLeft += 10; // Adjust the scroll speed as desired
-//   } else if (scrollPosition < scrollStartPosition) {
-//     carousel.scrollLeft -= 10; // Adjust the scroll speed as desired
-//   }
+  createCardHTML(project) {
+    return `
+            <div class="carousel__card">
+                <img src="${project.image}" alt="${project.title}">
+                <h1>${project.title}</h1>
+                <p>${project.description}</p>
+                <div class="actions">
+                    <a target="_blank" href="${project.liveLink}"><button>Live</button></a>
+                    <a target="_blank" href="${project.codeLink}"><button>Code</button></a>
+                </div>
+            </div>
+        `;
+  }
 
-//   scrollStartPosition = scrollPosition;
-// }
+  updateCarousel() {
+    console.log(this.currentIndex, this.projects[this.currentIndex]);
+    let newCardHTML = this.createCardHTML(this.projects[this.currentIndex]);
+    this.carouselElement.innerHTML = newCardHTML; // Replace the current card with the new one
+  }
+
+  nextProject() {
+    // Advance to the next project, if there is one
+
+    if (this.currentIndex < this.projects.length - 1) {
+      this.currentIndex++;
+      this.updateCarousel();
+    }
+  }
+
+  prevProject() {
+    // Go back to the previous project, if there is one
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateCarousel();
+    }
+  }
+}
+
+window.onload = function () {
+  const carouselElement = document.querySelector(".carousel");
+  const nextButton = document.querySelector(".nextButton");
+  const prevButton = document.querySelector(".prevButton");
+
+  new Carousel(projects, carouselElement, nextButton, prevButton);
+};
