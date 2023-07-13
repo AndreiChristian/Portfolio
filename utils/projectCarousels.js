c018a8446ee39706aaec43880627e43803121b9a
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-13 12:35:14 +0200

    >

--- utils/projectCarousels.js
+++ utils/projectCarousels.js
@@ -2,44 +2,46 @@ const projects = [
   {
     title: "Pupper palette",
     image: "../assets/images/beagle.png",
-    description: "Pupper Palette - an online store for digital art built with React.",
+    description:
+      "Pupper Palette - an online store for digital art built with React.",
     liveLink: "https://pupper-d9d8b.web.app/",
     codeLink: "https://github.com/username/project1",
   },
   {
-    title: "Pupper palette",
-    image: "../assets/images/beagle.png",
-    description: "This is the first project.",
-    liveLink: "https://pupper-d9d8b.web.app/",
-    codeLink: "https://github.com/username/project1",
+    title: "Pupper Rest",
+    image: "../assets/images/pupperRest.png",
+    description: "A lighweight REST API filled with canine data, built with Express and powered by a PostgreSQL",
+    liveLink: "https://expressjs-postgres-production-f8f8.up.railway.app/",
+    codeLink: "https://github.com/AndreiChristian/pupperRest",
   },
   {
     title: "I made a promise",
     image: "../assets/images/IMadeAPromise.png",
     description: "This is the second project.",
-    liveLink: "",
-    codeLink: "https://github.com/username/project2",
+    liveLink: "https://andreichristian.github.io/IMadeAPromise/",
+    codeLink: "https://github.com/AndreiChristian/IMadeAPromise",
   },
   {
     title: "Wiz Sort",
     image: "../assets/images/WizSort.png",
     description: "This is the second project.",
-    liveLink: "https://live-link-2.com",
-    codeLink: "https://github.com/username/project2",
+    liveLink: "https://sort-viz-portfolio.web.app/",
+    codeLink: "https://github.com/AndreiChristian/sortViz",
   },
   {
-    title: "4",
-    image: "assets/images/project2.png",
-    description: "This is the second project.",
-    liveLink: "https://live-link-2.com",
-    codeLink: "https://github.com/username/project2",
+    title: "LVX",
+    image: "../assets/images/LVX.png",
+    description:
+      "LVX is a project in development for a luxury villa company with two Angular Apps, two React Native Apps, and a backend built with Node, Express, PostgreSQL, Websockets",
+
+    codeLink: "https://github.com/AndreiChristian/ISH",
   },
   {
-    title: "5",
-    image: "assets/images/project2.png",
-    description: "This is the second project.",
-    liveLink: "https://live-link-2.com",
-    codeLink: "https://github.com/username/project2",
+    title: "Chip Chirip",
+    image: "../assets/images/chip.png",
+    description: "Chip Chirip is a chat app in development that includes a Angular front end and Node, Express, Websockets and PostgreSQL backend ",
+    // liveLink: "https://live-link-2.com",
+    codeLink: "https://github.com/AndreiChristian/chipchirip_server",
   },
 ];
 
@@ -64,8 +66,14 @@ class Carousel {
                 <h1>${project.title}</h1>
                 <p>${project.description}</p>
                 <div class="actions">
-                    <a target="_blank" href="${project.liveLink}"><button>Live</button></a>
-                    <a target="_blank" href="${project.codeLink}"><button>Code</button></a>
+                    ${
+                      project.liveLink
+                        ? `<a target="_blank" href="${project.liveLink}"><button>Live</button></a>`
+                        : `<span></span>`
+                    }
+                    <a target="_blank" href="${
+                      project.codeLink
+                    }"><button>Code</button></a>
                 </div>
             </div>
         `;
